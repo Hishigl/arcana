d873960fc90e510b428b8eb1f969ef6c7a32109a
Natasha <99311452+Hishigl@users.noreply.github.com>
2023-01-24 04:32:06 +0700

    main

--- check.php
+++ check.php
@@ -152,11 +152,11 @@ switch ($_GET['act']) {
 		  </div>";
     } else {
       echo "
-	 <h2 class='text text-primary'>Diagnosa Penyakit</h2>  <hr>
+	 <h2 class='text text-primary'>Diagnosa Tipe Kulit Wajah</h2>  <hr>
 	 <div class='alert alert-success alert-dismissible'>
                 <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>×</button>
                 <h4><i class='icon fa fa-exclamation-triangle'></i>Perhatian !</h4>
-                Silahkan memilih gejala sesuai dengan kondisi ayam anda, anda dapat memilih kepastian kondisi ayam dari pasti tidak sampai pasti ya, jika sudah tekan tombol proses (<i class='fa fa-search-plus'></i>)  di bawah untuk melihat hasil.
+                Silahkan memilih gejala sesuai dengan kondisi kulit wajah anda, anda dapat memilih tingkat kepastian kondisi kulit wajah anda dari pasti tidak sampai pasti ya, jika sudah tekan tombol proses (<i class='fa fa-search-plus'></i>)  di bawah untuk melihat hasil.
               </div>
 		<form name=text_form method=POST action='diagnosa' >
            <table class='table table-bordered table-striped konsultasi'><tbody class='pilihkondisi'>
