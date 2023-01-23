7182e8c6e5ae77b56a5925feb750e9b6d7ae7913
Natasha <99311452+Hishigl@users.noreply.github.com>
2023-01-24 04:27:21 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<title>Home</title>
+<?php include 'head.php';?>
+<?php include 'navbar.php';?>
+
+<body>
+    <div class="container">
+        <div class="row" style="margin-top:90px;">
+            <div class="col-sm" style="display: flex; justify-content: center;">
+                <img src="asset/img/tipe.jpg">
+            </div>
+            <div class="col-sm">
+                <h1 style="font-size:60px; font-family: 'Rowdies', cursive;">Ayo Cek Tipe Kulit Wajahmu di <span style="color:blue">Ar</span><span style="color:red">Ca</span>Na Gratis!</h1>
+                <p style="font-size:20px; margin-top:30px">Arcana membantumu menentukan jenis kulit wajah Anda sehingga dapat memudahkan anda dalam memilih produk kecantikan</p>
+            </div>
+        </div>
+        <div class="row" style="margin-top:40px; padding-left:40%;  padding-right:40%">
+            <button onClick="document.location='check.php'" style="auto;">
+                Cek Tipe Kulit Wajah
+            </button>
+        </div>
+    </div>
+</body>
+</html>
+
---
+++ navbar.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-expand-lg navbar-dark p-3" style="background-color:#F46E16; font-size:25px; font-family: 'Rowdies', cursive;" id="headerNav">
+    <div class="container-fluid">
+        <a class="navbar-brand d-block d-lg-none" href="#">
+        <img src="/static_files/images/logos/logo_2_white.png" height="80" />
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+    
+        <div class=" collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav mx-auto ">
+            <li class="nav-item">
+            <a class="nav-link mx-2 active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link mx-2" href="#">Skincare Products</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link mx-2" href="#">News</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link mx-2" href="#">About Us</a>
+            </li>
+            <!-- <li class="nav-item dropdown">
+                <a class="nav-link mx-2 dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Company
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="#">Blog</a></li>
+                    <li><a class="dropdown-item" href="#">About Us</a></li>
+                    <li><a class="dropdown-item" href="#">Contact us</a></li>
+                </ul>
+            </li> -->
+        </ul>
+        </div>
+    </div>
+    </nav>
