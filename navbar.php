4d2f02ccbf3b4bb82c6984878db336d406582db8
Natasha <99311452+Hishigl@users.noreply.github.com>
2023-01-24 04:34:14 +0700

    main

--- navbar.php
+++ navbar.php
@@ -10,7 +10,7 @@
         <div class=" collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav mx-auto ">
             <li class="nav-item">
-            <a class="nav-link mx-2 active" aria-current="page" href="#">Home</a>
+            <a class="nav-link mx-2 active" aria-current="page" href="index.php">Home</a>
             </li>
             <li class="nav-item">
             <a class="nav-link mx-2" href="#">Skincare Products</a>
