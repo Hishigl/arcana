2d74467f4a5c0476b6ec9af1923204cb6489c254
Natasha <99311452+Hishigl@users.noreply.github.com>
2023-01-24 16:24:25 +0700

    tes

--- check.php
+++ check.php
@@ -153,15 +153,15 @@ switch (isset($_GET['act'])) {
 		  </div>";
     } else {
       echo "
-	 <h2 class='text text-primary'>Diagnosa Tipe Kulit Wajah</h2>  <hr>
-	 <div class='alert alert-success alert-dismissible'>
-                <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>×</button>
-                <h4><i class='icon fa fa-exclamation-triangle'></i>Perhatian !</h4>
-                Silahkan memilih gejala sesuai dengan kondisi kulit wajah anda, anda dapat memilih tingkat kepastian kondisi kulit wajah anda dari pasti tidak sampai pasti ya, jika sudah tekan tombol proses (<i class='fa fa-search-plus'></i>)  di bawah untuk melihat hasil.
-              </div>
-		<form name=text_form method=POST action='diagnosa' >
-           <table class='table table-bordered table-striped konsultasi'><tbody class='pilihkondisi'>
-           <tr><th>No</th><th>Kode</th><th>Gejala</th><th width='20%'>Pilih Kondisi</th></tr>";
+        <h2 class='text text-primary'>Diagnosa Tipe Kulit Wajah</h2>  <hr>
+        <div class='alert alert-success alert-dismissible'>
+                      <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>×</button>
+                      <h4><i class='icon fa fa-exclamation-triangle'></i>Perhatian !</h4>
+                      Silahkan memilih gejala sesuai dengan kondisi kulit wajah anda, anda dapat memilih tingkat kepastian kondisi kulit wajah anda dari pasti tidak sampai pasti ya, jika sudah tekan tombol proses (<i class='fa fa-search-plus'></i>)  di bawah untuk melihat hasil.
+                    </div>
+          <form name=text_form method=POST action='diagnosa' >
+                <table class='table table-bordered table-striped konsultasi'><tbody class='pilihkondisi'>
+                <tr><th>No</th><th>Kode</th><th>Gejala</th><th width='20%'>Pilih Kondisi</th></tr>";
 
       $sql3 = mysqli_query($conn, "SELECT * FROM gejala order by kode_gejala");
       $i = 0;
