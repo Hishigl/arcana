c06fea4ec5f2ee3b8201751ae0812c0739c6dc09
Natasha <99311452+Hishigl@users.noreply.github.com>
2023-01-25 15:09:15 +0700

    main

--- check.php
+++ check.php
@@ -359,6 +359,22 @@ switch (isset($_GET['act'])) {
   }
 
 
+  .float{
+	position:fixed;
+	width:60px;
+	height:60px;
+	bottom:40px;
+	right:40px;
+	background-color:#0C9;
+	color:#FFF;
+	border-radius:50px;
+	text-align:center;
+	box-shadow: 2px 2px 3px #999;
+}
+
+.my-float{
+	margin-top:22px;
+}
   span.kondisipilih {
       background-color: #2f2130;
       padding: 2px 4px;
