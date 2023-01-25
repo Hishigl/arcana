ac7116c893415ffd49c8712125e44b8522b52c70
Natasha <99311452+Hishigl@users.noreply.github.com>
2023-01-25 15:05:49 +0700

    main

--- check.php
+++ check.php
@@ -274,6 +274,22 @@ switch (isset($_GET['act'])) {
       vertical-align:middle;
   }
 
+  .float{
+	position:fixed;
+	width:60px;
+	height:60px;
+	bottom:40px;
+	right:40px;
+	background-color:#0C9;
+	color:#FFF;
+	border-radius:50px;
+	text-align:center;
+	box-shadow: 2px 2px 3px #999;
+}
+
+.my-float{
+	margin-top:22px;
+}
   tbody.pilihkondisi td,
   tbody.pilihkondisi td.gejala,
   tbody.pilihkondisi th{
